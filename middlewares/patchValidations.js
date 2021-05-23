47e6c687606c4987b3413ac7fcf5e2eb9663e492
JeremiasA <jeremiasamestoy@gmail.com>
2021-05-23 18:56:41 -0300

    valid categories obtained from database

--- middlewares/patchValidations.js
+++ middlewares/patchValidations.js
@@ -1,31 +1,22 @@
 
 const { Category } = require("../database/connection");
-
-const categories_options = {  
-    1: "Frenos",
-    2: "Suspensión",
-    3: "Electricidad",
-    4: "Interiores",
-    5: "Motor",
-    6: "General",
-    7: "Calefacción",
-};
-
-const validate = {
-
-    category: async (req, res, next) => {
-        const categories = await Category.findOne({
-            where: { id: req.body.CategoryId },
-        });
-        if (!categories) {
-            req.categoryError = {};
-            req.categoryError = {
-                "Invalid category ID": `Select from:`,
-                categories_options,
-            };
-        }
-        next();
-        return;
+    
+    const validate = {
+        
+        category: async (req, res, next) => {
+            const categories_options = await Category.findAll({attributes:["id", "name"]}) 
+            const categories = await Category.findOne({
+                where: { id: req.body.CategoryId },
+            });
+            if (!categories) {
+                req.categoryError = {};
+                req.categoryError = {
+                    "Invalid category ID": `Select from:`,
+                    categories_options,
+                };
+            }
+            next();
+            return;
     }
     
 };
