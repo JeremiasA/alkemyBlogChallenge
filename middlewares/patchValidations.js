2f72f25df6e3fb288ed532a3f814534bf09a0235
JeremiasA <jeremiasamestoy@gmail.com>
2021-05-28 04:08:09 -0300

    identation fix

--- middlewares/patchValidations.js
+++ middlewares/patchValidations.js
@@ -10,28 +10,28 @@ module.exports =
  body("title")
     .optional()
     .not().isEmpty()
-        .withMessage("Title can't be empty"),
+    .withMessage("Title can't be empty"),
 body("content")
     .optional()
-    .not().isEmpty().
-        withMessage("Content can't be empty"),
+    .not().isEmpty()
+    .withMessage("Content can't be empty"),
     
 body("CategoryId")
-        .optional()
-        .not().isEmpty()
-        .isNumeric()
-        .custom((value, {req}) => {
-            return Category.findOne({where : {id:value}}).then(user => {
-                if (!user) {
-                return Promise.reject('Invalid category id');
-                }
-            });
-            }),
+    .optional()
+    .not().isEmpty()
+    .isNumeric()
+    .custom((value, {req}) => {
+        return Category.findOne({where : {id:value}}).then(category => {
+            if (!category) {
+            return Promise.reject('Invalid category id');
+            }
+        });
+        }),
 
 body("image")
     .optional()
     .matches(/\.png{1}$|\.jpg{1}$/i)
-        .withMessage("Image src must end with .jpg or .png"),
+    .withMessage("Image src must end with .jpg or .png"),
         
 function(req,res,next) {
             next()
